d61033a9fc46e99ff177615d6551e8a8df69fb3d
Scott Aaron <dr_saaron@yahoo.com>
2021-06-04 14:00:25 -0500

    close the window nicely

--- src/main/java/com/blazartech/yahtzee/YahtzeeWindowCommandLineRunner.java
+++ src/main/java/com/blazartech/yahtzee/YahtzeeWindowCommandLineRunner.java
@@ -7,6 +7,8 @@ package com.blazartech.yahtzee;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -20,17 +22,35 @@ import org.springframework.stereotype.Component;
 public class YahtzeeWindowCommandLineRunner implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(YahtzeeWindowCommandLineRunner.class);
+
     @Override
     public void run(String... args) throws Exception {
         logger.info("opening window");
-        
+
         Frame mainWindow = new Frame("Yahtzee");
+        mainWindow.addWindowListener(new WindowCloseListener(mainWindow));
         mainWindow.setLayout(new BorderLayout());
         GameDice dice = new GameDice();
         ScoreCard scoreCard = new ScoreCard(dice);
         mainWindow.add(dice, BorderLayout.NORTH);
         mainWindow.add(scoreCard, BorderLayout.CENTER);
         mainWindow.pack();
-        mainWindow.setVisible(true);    }
+        mainWindow.setVisible(true);
+    }
     
+    static class WindowCloseListener extends WindowAdapter {
+        
+        private final Frame mainWindow;
+
+        public WindowCloseListener(Frame mainWindow) {
+            this.mainWindow = mainWindow;
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            mainWindow.dispose();
+        }
+        
+        
+    }
 }
